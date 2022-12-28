c755aed314aa089d087eeb0b0e95fa427c5443f4
vtiansw <shanwtian@163.com>
2022-12-28 20:55:59 +0800

    vite-project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {defineComponent} from 'vue'
+import './assets/hello.css'
+
+const Test = defineComponent({
+    render() {
+        return(
+            <div class='title'> Hello World </div>
+        )
+    }
+}
+)
+
+export default Test
---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import {defineConfig} from 'vite'
+import vue from '@vitejs/plugin-vue'  // <--- import vue plugin, 使用HMR项目热更新, 热更新不会刷新页面, 会保留当前页面状态, 但是会重新渲染页面
+import vueJsxPlugin from '@vitejs/plugin-vue-jsx' // <--- import vue-jsx plugin, 使用jsx语法, 但是需要安装依赖, npm i @vue/babel-plugin-jsx -D, 然后在babel.config.js中配置
+import viteLegacyPlugin from "@vitejs/plugin-legacy";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+        plugins: [vue(),
+            vueJsxPlugin(),
+            viteLegacyPlugin({targets: ['defaults']})],
+        optimizeDeps: {
+            // include:['esm-dep']
+            build: {
+                target: 'es2015',
+            },
+        },
+    }
+);
